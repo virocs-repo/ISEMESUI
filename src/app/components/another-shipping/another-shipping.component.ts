5abe0738d12fca7df3f8abcad5d252609f1e9947
ShilpaReddy06 <shilpav@virocs.com>
2024-11-12 21:41:53 +0530

    Added extra tabs

---
+++ src/app/components/another-shipping/another-shipping.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-another-shipping',
+  templateUrl: './another-shipping.component.html',
+  styleUrls: ['./another-shipping.component.scss']
+})
+export class AnotherShippingComponent {
+
+}
